0a93b3c12ae5a00f42e14b4a67d16fda5918187c
ridwanmaulanaavana <ridwan.maulana.programmer@gmail.com>
2022-12-07 02:30:37 +0700

    udah diperbaiki masalah bug tadi dari diskon gila-gilaan menjadi diskon okai

--- gofood.go
+++ gofood.go
@@ -3,8 +3,8 @@ package main
 func main(){
 	//ini untuk menjalankan aplikasi
 	//ini untuk ngefix bug
-	//ini fitur go food dengan diskon gila"an..
+	//ini fitur go food dengan diskon yang okai
 	//Dimana skrng kita akan belajar bener" sampe berhasil
 	//gofood skrng udah bsa ngasih rekomendasi 100% akurat
-	
+
 }
