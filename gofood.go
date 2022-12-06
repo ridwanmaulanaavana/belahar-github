1706c4dd84bacfcce4fcdf35e448029001be7652
ridwanmaulanaavana <ridwan.maulana.programmer@gmail.com>
2022-12-07 02:04:02 +0700

    menambahkan fitur untuk gofood

---
+++ gofood.go
@@ -0,0 +1,8 @@
+package main
+
+func main(){
+	//ini untuk menjalankan aplikasi
+	//ini untuk ngefix bug
+	//ini fitur go food dengan diskon gila"an..
+	//Dimana skrng kita akan belajar bener" sampe berhasil
+}
