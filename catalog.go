3276c56733f8839828fa466ae18b9996245c92c7
ridwan maulana <116152516+ridwanmaulanaavana@users.noreply.github.com>
2022-12-07 02:50:33 +0700

    Update catalog.go

--- catalog.go
+++ catalog.go
@@ -1,4 +1,7 @@
 package main {
 // ini adalah file untuk catalog
+  // ini adalah kalileran yg sangat nyata
+  // maka kita harus bener" struggle dan hard work for this job
+  // menyukai pekerjaan adalah hal yang sangat penting dalam kehidupan kita dimasa yang akan datang
 
 }
